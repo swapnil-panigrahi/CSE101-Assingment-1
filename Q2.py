1af8ed8ea71157323e5d8b9d2765ed7fc24977f7
critz㌹ <pokemonmasterswapnil@gmail.com>
2022-12-08 15:21:16 +0530

    Changed Q2 to make it more cancer than it already was

--- Q2.py
+++ Q2.py
@@ -5,7 +5,17 @@ for x in range(min(400//8,120//2)+1):                                           
     y2=120-2*x                                                                         #solving y for both linear equations
         
     if y1==y2 or min(y1,y2)==0 or x==0:                                                #checking if optimal solutions are acquired for the given value of x and adding it to the list
-        optimal_solution.append([(x,min(y1,y2)),90*x+25*min(y1,y2)])                   #solution are added to the list in the format [(x,y),z] where z is the profit
+        
+        if x>M and min(y1,y2)>M:
+            profit=100*(x-M)+90*M+30*(min(y1,y2)-M)+25*M
+        
+        elif x<=M and min(y1,y2)>M:
+            profit=90*x+30*(min(y1,y2)-M)+25*M
+        
+        else:
+            profit=90*x+25*min(y1,y2)                                   
+
+        optimal_solution.append([(x,min(y1,y2)),profit])                               #solution are added to the list in the format [(x,y),z] where z is the profit
 
 else:                                                                                  #after the for loop is completed, else block is executed
     max=0
