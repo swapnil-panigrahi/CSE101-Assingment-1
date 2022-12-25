5efeb517e53796d321d3b144885a091ae52416fd
critz㌹ <pokemonmasterswapnil@gmail.com>
2022-12-25 12:33:09 +0530

    Fixed a few bugs

--- Q7.py
+++ Q7.py
@@ -3,7 +3,7 @@ allowance=int(input("Enter monthly allowance: "))
 sf=float(input("Saving fraction: "))
 r=float(input("Enter rate of interest: "))
 
-savings=2000
+savings=allowance*sf
 months=1
 
 while cost>savings:
@@ -16,10 +16,17 @@ else:
 print(months, "Months")
 print(round(savings-cost,3), "will be left as savings!")
 
+savings=allowance*sf
+
 def bonus(cost,allowance,r):
     months=int(input("Enter number of months: "))
-    sf=(cost/(allowance*((1+r/100)**(months)-1)))*(r/100)
+    sf=(cost/(allowance*((1+r/100)**(months-1)-1)))*(r/100)
     
     return sf
 
-print("Saving fractions should be:",bonus(cost,allowance,r))
+saving_frac=bonus(cost,allowance,r)
+
+if saving_frac>1:
+    print("Not possible")
+else:
+    print("Saving fractions should be:",saving_frac)
